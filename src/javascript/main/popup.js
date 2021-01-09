5281c111306736a96fa68caa1e6fcc1b1752b9b7
tik-86 <passatik11@gmail.com>
2021-01-09 16:24:23 +0200

    first commit

---
+++ src/javascript/main/popup.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function togglePopup(ClassName) {
+        var popupClose = document.querySelector(ClassName);
+        popupClose.classList.toggle('popup_show');
+    }
+    function closePopup() {
+        var popupClose = document.querySelectorAll('.popup');
+        for (var i = 0; i < popupClose.length; i++) {
+            var obj2 = popupClose[i];
+            obj2.classList.remove('popup_show');
+        }
+    }
+    function showPopupEvent() {
+        var btnsItems = document.querySelectorAll('.js__show-popup');
+        for (var i = 0; i < btnsItems.length; i++) {
+            var obj3 = btnsItems[i];
+            obj3.addEventListener('click', function (e) {
+                e.preventDefault();
+                var currentPopup = this.getAttribute('data-popup');
+                togglePopup('.' + currentPopup)
+            });
+        }
+    }
+    showPopupEvent();
+    function closeBtns() {
+        var closeButton = document.querySelectorAll('.js__close-popup');
+        for (var i = 0; i < closeButton.length; i++) {
+            var obj2 = closeButton[i];
+            obj2.addEventListener('click', function (e) {
+                e.preventDefault();
+                closePopup()
+            })
+        }
+    }
+    closeBtns();
+});
+
+
